e2c47e0edef8f6fc70346bab4dd4a640a9aeb1b6
cesar3963 <67514872+cesar3963@users.noreply.github.com>
2020-08-29 15:26:16 -0400

    Update python-challange.py

--- python-challange.py
+++ python-challange.py
@@ -1,11 +1,11 @@
 import csv
 import os
-csvpath = os.path.join("budget_data.csv")
+csvpath = os.path.join('PyBank','budget_data.csv')
 
    
-open(csvpath, newline='') as csvfile:
-csvreader =.csv.reader(csvfile, delimiter=',')
-print(csvreader)
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvpath, delimiter=',')
+    print(csvreader)
 
 for row in csvreader:
     print(row)
