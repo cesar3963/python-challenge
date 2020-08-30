be6d161667a8affa01f360c7e5a8fde1b7649d3d
cesar3963 <67514872+cesar3963@users.noreply.github.com>
2020-08-30 01:06:36 -0400

    done

--- python-challange.py
+++ python-challange.py
@@ -1,11 +1,17 @@
-import csv
 import os
+import csv
 
-csvpath = os.path.joim("..", "csv", "budget_data.csv")
+
+csvpath = os.path.joim("csv", "budget_data.csv")
 
 
-with open(csvpath,newline="") as csvfile:
+with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    for row in csvreader:count(row)
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"csv header: {csv_header}")
+
+    for row in csvreader:
         print(row)
