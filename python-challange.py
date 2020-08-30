f97aa4a5c123599fa8f43915422ba0986bf92b6a
cesar3963 <67514872+cesar3963@users.noreply.github.com>
2020-08-29 23:08:38 -0400

    j

--- python-challange.py
+++ python-challange.py
@@ -1,4 +1,11 @@
 import csv
 import os
 
-csvpath = os.path.joim()
+csvpath = os.path.joim("..", "csv", "budget_data.csv")
+
+
+with open(csvpath,newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    for row in csvreader:count(row)
+        print(row)
