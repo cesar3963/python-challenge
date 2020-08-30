e1d62daa3fd0cb8fd49fcfda9f578243c72f7050
cesar3963 <67514872+cesar3963@users.noreply.github.com>
2020-08-29 21:21:07 -0400

    hi

--- python-challange.py
+++ python-challange.py
@@ -1,11 +1,10 @@
 import csv
 import os
-csvpath = os.path.join('PyBank','budget_data.csv')
+csvpath = os.path.join('budget_data.csv')
 
    
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvpath, delimiter=',')
-    print(csvreader)
-
-for row in csvreader:
-    print(row)
+    date = 0
+    for row in csvreader:
+        print(', '.join(row))
